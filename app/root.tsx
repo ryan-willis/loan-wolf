66f094d8e07a19275f90a2ffd85088dcf799843d
Ryan Willis <code@ryanwillis.com>
2024-05-21 14:36:19 -0700

    add logo to all pages

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,8 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import logo from "~/assets/logo.svg";
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -25,5 +27,19 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "start",
+          alignItems: "center",
+        }}
+      >
+        <img width={64} height={64} src={logo} alt="Loan Wolf Logo" />
+        <h1>LOAN WOLF</h1>
+      </div>
+      <Outlet />
+    </>
+  );
 }
