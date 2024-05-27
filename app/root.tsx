13c1dfb55c9f2a8e5e875c79426c3fe9a638f84c
Ryan Willis <code@ryanwillis.com>
2024-05-27 13:04:24 -0700

    adjust the logo so it's just the head

--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,8 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-import logo from "~/assets/logo.svg";
+// import logo from "~/assets/logo.svg";
+import logo from "~/assets/head.svg";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -34,10 +35,11 @@ export default function App() {
           display: "flex",
           justifyContent: "start",
           alignItems: "center",
+          gap: ".5rem",
         }}
       >
-        <img width={64} height={64} src={logo} alt="Loan Wolf Logo" />
-        <h1>LOAN WOLF</h1>
+        <img width={48} height={48} src={logo} alt="Loan Wolf Logo" />
+        <h1 style={{ fontSize: "1.75rem" }}>LOAN WOLF</h1>
       </div>
       <Outlet />
     </>
